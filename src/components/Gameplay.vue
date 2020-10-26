fc916437a9704f318582657fb6af809450d65c45
Laurell Seville <laurellseville2000@gmail.com>
2020-10-25 22:44:15 -0500

    questions and answers are working

--- src/components/Gameplay.vue
+++ src/components/Gameplay.vue
@@ -17,9 +17,16 @@
           single-line
           hide-details
           class="input mt-5"
+          v-model="answer"
         ></v-text-field>
 
+        {{ answer }}
+
         <v-btn class="mt-5" dark v-on:click="changeStage"> Submit </v-btn>
+
+        <v-alert :value="alert" type="error" class="mt-5">
+          Incorrect!
+        </v-alert>
 
         <div class="counter mt-5 center">
           <p>Count down</p>
@@ -42,23 +49,47 @@ export default {
   },
   methods: {
     changeStage: function () {
+      console.log(this.answer == this.stages[this.currentStage - 1].text);
+
+      this.alert = false;
+
+      if (this.answer == this.stages[this.currentStage - 1].text) {
+        this.moveOnToNextStage();
+        this.resetCounter();
+      } else {
+        this.alert = !this.alert;
+      }
+    },
+
+    moveOnToNextStage: function () {
       if (this.currentStage + 1 <= 3) {
         this.currentStage += 1;
       } else {
         this.currentStage = 1;
       }
     },
+    resetCounter: function () {
+      this.stages[this.currentStage - 1].counter = 30;
+    },
     beginStage: function () {
       setInterval(() => {
         this.stages[this.currentStage - 1].counter -= 1;
-        if(this.stages[this.currentStage - 1].counter == 0 ) {
-          this.currentStage += 1;
+        if (
+          this.stages[this.currentStage - 1].counter == 0 &&
+          this.currentStage <= 2 && this.answer == this.stages[this.currentStage - 1].text
+        ) {
+          this.moveOnToNextStage();
+        }
+        if (this.stages[this.currentStage - 1].counter == 0 && this.answer !== this.stages[this.currentStage - 1].text ) {
+          console.log("Game Failed")
         }
       }, 1000);
     },
   },
   data() {
     return {
+      alert: false,
+      answer: "",
       stages: [
         {
           number: 1,
@@ -67,14 +98,12 @@ export default {
         },
         {
           number: 2,
-          text: `Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
-          `,
+          text: `Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur`,
           counter: 30,
         },
         {
           number: 3,
-          text: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-          `,
+          text: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.`,
           counter: 30,
         },
       ],
--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import vuetify from './plugins/vuetify';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import Vuetify from 'vuetify/lib'
+import '@mdi/font/css/materialdesignicons.css'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
-
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
